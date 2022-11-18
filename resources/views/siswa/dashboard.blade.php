a3e9d4fa34fcec392cff19872e31d2a743fca1d3
talitha <talithasyafiyah1112@gmail.com>
2022-11-18 09:00:57 +0700

    talitha - revisi dashboard siswa

--- resources/views/siswa/dashboard.blade.php
+++ resources/views/siswa/dashboard.blade.php
@@ -23,118 +23,114 @@
         <section class="row">
             <div class="col-12">
                 <div class="row mb-3">
-                    <div class="col-12 col-lg-12 col-md-12">
-                        <div class="alert alert-danger">
-                            <h4 class="alert-heading">Tagihan SPP</h4>
-                            <h6 class="alert-heading mb-3">Rp 2.250.000</h6>
-                            <div class="col-12">
-                                <div class="">
-                                    <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal"
-                                        data-bs-target="#inlineForm">
-                                        Konfirmasi SPP
-                                    </button>
-                                    {{-- Modal --}}
-                                    <div class="modal fade text-left" id="inlineForm" tabindex="-1" role="dialog"
-                                        aria-labelledby="myModalLabel33" aria-hidden="true">
-                                        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
-                                            role="document">
-                                            <div class="modal-content">
-                                                <div class="modal-header bg-success">
-                                                    <h4 class="modal-title white" id="myModalLabel33">Konfirmasi SPP</h4>
-                                                    <button type="button" class="close" data-bs-dismiss="modal"
-                                                        aria-label="Close">
-                                                        <i data-feather="x"></i>
-                                                    </button>
+                    <div class="col-lg-4 col-md-12">
+                        <div class="card bg-danger">
+                            <div class="card-body">
+                                <div class="row">
+                                    <h4 class="alert-heading text-light">Tagihan SPP</h4>
+                                    <h6 class="alert-heading mb-3 text-light">Rp 2.250.000</h6>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-12 mt-2">
+                                        <button class="btn btn-primary btn-sm"><i class="bi bi-card-list"></i> &nbspDaftar Tagihan</button>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12 mt-2">
+                                        <button type="button" class="btn btn-success btn-sm" data-bs-toggle="modal"
+                                            data-bs-target="#inlineForm">
+                                            <i class="bi bi-file-check"></i> &nbspKonfirmasi SPP
+                                        </button>
+                                        {{-- Modal --}}
+                                        <div class="modal fade text-left" id="inlineForm" tabindex="-1" role="dialog"
+                                            aria-labelledby="myModalLabel33" aria-hidden="true">
+                                            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable"
+                                                role="document">
+                                                <div class="modal-content">
+                                                    <div class="modal-header bg-success">
+                                                        <h4 class="modal-title white" id="myModalLabel33">Konfirmasi SPP</h4>
+                                                        <button type="button" class="close" data-bs-dismiss="modal"
+                                                            aria-label="Close">
+                                                            <i data-feather="x"></i>
+                                                        </button>
+                                                    </div>
+                                                    <form action="#">
+                                                        <div class="modal-body">
+                                                            <label>Metode Pembayaran: </label>
+                                                            <div class="form-group">
+                                                                <input type="text" placeholder="Metode Pembayaran"
+                                                                    class="form-control">
+                                                            </div>
+                                                            <label>Bukti Pembayaran: </label>
+                                                            <div class="form-group">
+                                                                    <input class="form-control" type="file" id="formFile">
+                                                            </div>
+                                                        </div>
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-light-secondary"
+                                                                data-bs-dismiss="modal">
+                                                                <i class="bx bx-x d-block d-sm-none"></i>
+                                                                <span class="d-none d-sm-block">Tutup</span>
+                                                            </button>
+                                                            <button type="button" class="btn btn-success ml-1"
+                                                                data-bs-dismiss="modal">
+                                                                <i class="bx bx-check d-block d-sm-none"></i>
+                                                                <span class="d-none d-sm-block">Simpan</span>
+                                                            </button>
+                                                        </div>
+                                                    </form>
                                                 </div>
-                                                <form action="#">
-                                                    <div class="modal-body">
-                                                        <label>Metode: </label>
-                                                        <div class="form-group">
-                                                            <input type="text" placeholder="Mata Pelajaran"
-                                                                class="form-control">
-                                                        </div>
-                                                        <label>Bukti Pembayaran: </label>
-                                                        <div class="form-group">
-                                                            <input type="text" placeholder="Kelas"
-                                                                class="form-control">
-                                                        </div>
-                                                    </div>
-                                                    <div class="modal-footer">
-                                                        <button type="button" class="btn btn-light-secondary"
-                                                            data-bs-dismiss="modal">
-                                                            <i class="bx bx-x d-block d-sm-none"></i>
-                                                            <span class="d-none d-sm-block">Tutup</span>
-                                                        </button>
-                                                        <button type="button" class="btn btn-success ml-1"
-                                                            data-bs-dismiss="modal">
-                                                            <i class="bx bx-check d-block d-sm-none"></i>
-                                                            <span class="d-none d-sm-block">Simpan</span>
-                                                        </button>
-                                                    </div>
-                                                </form>
                                             </div>
                                         </div>
                                     </div>
-                                    <button class="btn btn-primary btn-sm">Lihat Tagihan</button>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="card">
-                    <div class="card-body">
-                        <div class="row">
-                            <h4>Profil Singkat</h4>
-                            <div class="col-lg-3 col-md-6">
-                                <div class="pt-3">
-                                    <img src="assets/images/Abby.jpg" class="rounded" alt="..." width="160">
-                                </div>
-                            </div>
-                            <div class="col-9">
-                                <div class="pt-3">
-                                    <form class="form form-horizontal">
-                                        <div class="form-body">
-                                            <div class="row">
-                                                <div class="col-md-3">
-                                                    <label>Nama</label>
-                                                </div>
-                                                <div class="col-md-9 form-group">
-                                                    <input type="text" id="nama" class="form-control" name="nama"
-                                                        placeholder="Talitha Syafiyah" readonly>
-                                                </div>
-                                                <div class="col-md-3">
-                                                    <label>NIS</label>
-                                                </div>
-                                                <div class="col-md-9 form-group">
-                                                    <input type="number" id="nis-id" class="form-control" name="nis-id"
-                                                        placeholder="211402018" readonly>
-                                                </div>
-                                                <div class="col-md-3">
-                                                    <label>Jenis Kelamin</label>
-                                                </div>
-                                                <div class="col-md-9 form-group">
-                                                    <input type="text" id="jenis_kelamin" class="form-control" name="jenis_kelamin"
-                                                        placeholder="Perempuan" readonly>
-                                                </div>
-                                                <div class="col-md-3">
-                                                    <label>Tanggal Lahir</label>
-                                                </div>
-                                                <div class="col-md-9 form-group">
-                                                    <input type="text" id="ttl" class="form-control" name="ttl"
-                                                        placeholder="11/12/2003" readonly>
-                                                </div>
-                                                <div class="col-md-3">
-                                                    <label>Tempat Lahir</label>
-                                                </div>
-                                                <div class="col-md-9 form-group">
-                                                    <input type="text" id="tempat" class="form-control" name="tempat"
-                                                        placeholder="Medan" readonly>
-                                                </div>
-                                            </div>
+                    {{-- Profil Singkat --}}
+                    <div class="col-lg-8 col-md-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <h4>Profil Singkat</h4>
+                                <div class="row">
+                                    <div class="col-lg-3 col-md-12">
+                                        <div class="pt-3">
+                                            <img src="assets/images/Abby.jpg" class="rounded mx-auto d-block" alt="..." width="130">
                                         </div>
-                                    </form>
+                                    </div>
+
+                                    <div class="col-lg-9 col-md-12">
+                                        <div class="pt-3">
+                                            <table class="table mb-0">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Nama</td>
+                                                        <td>:</td>
+                                                        <td>Abby Syafiyah</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>NIS</td>
+                                                        <td>:</td>
+                                                        <td>211402018</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Jenis Kelamin</td>
+                                                        <td>:</td>
+                                                        <td>Perempuan</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Tanggal Lahir</td>
+                                                        <td>:</td>
+                                                        <td>11/12/2003</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Tempat Lahir</td>
+                                                        <td>:</td>
+                                                        <td>Medan</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </div>
+                                    </div>
                                 </div>
-                                
                             </div>
                         </div>
                     </div>
