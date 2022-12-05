543a5658a430fd63fda8b89760d1a1521120ba82
talitha <talithasyafiyah1112@gmail.com>
2022-12-05 23:49:59 +0700

    talithatrigger37-39

--- database/migrations/38_trigger_kontrak.php
+++ database/migrations/38_trigger_kontrak.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -13,7 +14,38 @@ return new class extends Migration
      */
     public function up()
     {
-        //
+        /* log insert kelas */
+        DB::unprepared('
+        CREATE TRIGGER log_insert_kontrak
+        AFTER INSERT ON kontrak_semesters
+        FOR EACH ROW
+        BEGIN
+        INSERT INTO log_kontraks(kontrak_semester_id, siswa, grade, semester, tahun_ajaran, status, action, created_at)
+        VALUES (NEW.kontrak_semester_id, NEW.siswa, NEW.grade, NEW.semester, NEW.tahun_ajaran, NEW.status, "insert", NOW());
+        END
+        ');
+
+        /* log update kelas */
+        DB::unprepared('
+        CREATE TRIGGER log_update_kontrak
+        AFTER UPDATE ON kontrak_semesters
+        FOR EACH ROW
+        BEGIN
+        INSERT INTO log_kontraks(kontrak_semester_id, siswa, grade, semester, tahun_ajaran, status, action, created_at)
+        VALUES (NEW.kontrak_semester_id, NEW.siswa, NEW.grade, NEW.semester, NEW.tahun_ajaran, NEW.status, "update", NOW());
+        END
+        ');
+
+        /* log delete kelas */
+        DB::unprepared('
+        CREATE TRIGGER log_delete_kontrak
+        AFTER DELETE ON kontrak_semesters
+        FOR EACH ROW
+        BEGIN
+        INSERT INTO log_kontraks(kontrak_semester_id, siswa, grade, semester, tahun_ajaran, status, action, created_at)
+        VALUES (NEW.kontrak_semester_id, NEW.siswa, NEW.grade, NEW.semester, NEW.tahun_ajaran, NEW.status, "delete", NOW());
+        END
+        ');
     }
 
     /**
@@ -23,6 +55,8 @@ return new class extends Migration
      */
     public function down()
     {
-        //
+        DB::unprepared('DROP TRIGGER log_insert_kontrak');
+        DB::unprepared('DROP TRIGGER log_update_kontrak');
+        DB::unprepared('DROP TRIGGER log_delete_kontrak');
     }
 };
