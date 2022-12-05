df584a07c20000e1fdeb1a0e4193ec60930e4124
Dzakiy Dzakwan <dzakcart@gmail.com>
2022-12-06 06:42:45 +0700

    1 more

--- database/migrations/22_create_log_roster_table.php
+++ database/migrations/22_create_log_roster_table.php
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('log_roster_kelas');
+        Schema::dropIfExists('log_roster');
     }
 };
