2149d35e63af38209b7d4c7be44e1d84783d02da
talitha <talithasyafiyah1112@gmail.com>
2022-12-06 22:09:10 +0700

    talitha-triggerLagi

--- database/migrations/40_trigger_nilai.php
+++ database/migrations/40_trigger_nilai.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration
 {
@@ -62,7 +63,7 @@ return new class extends Migration
             ELSEIF (NEW.nilai_pengetahuan > 100 OR NEW.nilai_keterampilan > 100) THEN
                 SIGNAL SQLSTATE "45000" SET MESSAGE_TEXT = "Error Nilai tidak dapat lebih dari 100";
             END IF;
-        END;
+        END
         ');
     }
 
