2821da80f64f1f63c54b0d32b3d7336fa2be29c8
talitha <talithasyafiyah1112@gmail.com>
2022-12-10 09:59:53 +0700

    table modal

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -67,7 +67,41 @@
                     <h4 class="modal-title white " id="myModalLabel33">Inactive Admin</h4>
                 </div>
                 <div class="modal-body">
-
+                    <table class="table table-bordered text-center" id="table1">
+                        <thead>
+                            <tr>
+                                <th>No</th>
+                                <th>Username</th>
+                                <th>Deleted_at</th>
+                                <th>Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>admin</td>
+                                <td>2003-12-12</td>
+                                <td>
+                                    <div class="modal-danger me-1 mb-1 d-inline-block">
+                                        <button type="button" class="btn btn-sm btn-success" data-bs-toggle="modal"
+                                            data-bs-target="#restore">
+                                                <div data-bs-toggle="tooltip" data-bs-placement="top" title="Restore">
+                                                    <i class="bi bi-arrow-repeat"></i></i>
+                                                </div>
+                                        </button>
+                                    </div>
+                                    <div class="modal-danger me-1 mb-1 d-inline-block">
+                                        <button type="button" class="btn btn-sm btn-danger" data-bs-toggle="modal"
+                                            data-bs-target="#delete">
+                                                <div data-bs-toggle="tooltip" data-bs-placement="top" title="Hapus">
+                                                    <i class="bi bi-trash-fill"></i></i>
+                                                </div>
+                                        </button>
+                                    </div>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
@@ -79,6 +113,56 @@
         </div>
     </div>
 
+    {{-- Modal Restore --}}
+    <div class="modal fade text-left" id="restore" tabindex="-1" role="dialog"
+        aria-labelledby="myModalLabel130" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-success">
+                    <h5 class="modal-title white" id="myModalLabel130">
+                        Restore
+                    </h5>
+                </div>
+                <div class="modal-body">Apakah anda yakin ingin mengembalikan akun?</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                        <i class="bx bx-x d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Close</span>
+                    </button>
+                    <button class="btn btn-success ml-1" data-bs-dismiss="modal" wire:click="">
+                        <i class="bx bx-check d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Restore</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {{-- Modal Delete permanen --}}
+
+    <div class="modal fade text-left" id="delete" tabindex="-1" role="dialog"
+        aria-labelledby="myModalLabel130" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable" role="document">
+            <div class="modal-content">
+                <div class="modal-header bg-danger">
+                    <h5 class="modal-title white" id="myModalLabel130">
+                        Hapus
+                    </h5>
+                </div>
+                <div class="modal-body">Apakah anda yakin ingin menghapus permanen?</div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-light-secondary" data-bs-dismiss="modal">
+                        <i class="bx bx-x d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Close</span>
+                    </button>
+                    <button class="btn btn-danger ml-1" data-bs-dismiss="modal" wire:click="">
+                        <i class="bx bx-check d-block d-sm-none"></i>
+                        <span class="d-none d-sm-block">Hapus</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
 
 @section('script')
