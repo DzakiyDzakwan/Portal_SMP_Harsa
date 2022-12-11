209134c922ac47835f8eaf960319266cfaea5304
talitha <talithasyafiyah1112@gmail.com>
2022-12-12 00:57:17 +0700

    talitha-updateSiswaLivewire

--- app/Http/Livewire/EditModalSiswa.php
+++ app/Http/Livewire/EditModalSiswa.php
@@ -4,22 +4,68 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\User;
+use App\Models\Siswa;
+use App\Models\UserProfile;
+use App\Models\LogActivity;
 
 class EditModalSiswa extends Component
 {
-    public $uuid, $nis, $nama, $data;
+    public $siswas, $nisn, $nis;
+
+    protected $listeners = [
+        'editUser' => 'showModal'
+    ];
+
+    protected $rules = [
+        'nama' => 'required',
+        'nis' => 'required|max:4'
+    ];
+
+    public function updated($fields) {
+        $this->validateOnly($fields);
+    }
 
     public function render()
     {
-
+        $this->siswas = User::join('siswas', 'siswas.user', '=', 'users.uuid')->join('user_profiles', 'user_profiles.user', '=', 'users.uuid')->get();
         return view('admin.components.livewire.edit-modal-siswa');
     }
 
-    public function showSiswa($uuid) {
-        $data = User::join('siswas', 'siswas.user', '=', 'users.uuid')->join('user_profiles', 'users.uuid', '=', 'user_profiles.user')->orderBy('siswas.created_at', 'DESC')->where('user', $uuid)->first();
-        // $this->uuid = $data->uuid;
-        $this->nis = $data->nis;
+    public function showModal($id) {
+        // dd($id);
+        $data = User::join('siswas', 'siswas.user', '=', 'users.uuid')->join('user_profiles', 'user_profiles.user', '=', 'users.uuid')->where('siswas.user', $id)->first();
+        $this->user = $id;
         $this->nama = $data->nama;
-        $this->dispatchBrowserEvent('update-modal');
+        $this->nisn = $data->NISN;
+        $this->nis = $data->NIS;
+
+        $this->dispatchBrowserEvent('edit-modal');
+    }
+
+    public function update()
+    {
+        $this->validate([
+            'nama' => 'required',
+            'nis' => 'required|max:4'
+        ]);
+
+        Siswa::where('user', $this->user)->update([
+            'NIS' => $this->nis,
+        ]);
+
+        UserProfile::where('user', $this->user)->update([
+            'nama' => $this->nama,
+        ]);
+
+        LogActivity::create([
+            'actor' => auth()->user()->uuid,
+            'action' => 'update',
+            'at' => 'siswas'
+        ]);
+
+        $this->emit('siswaUpdate');
+        $this->dispatchBrowserEvent('update-alert');
+        $this->dispatchBrowserEvent('edit-modal');
+        $this->render();
     }
 }
--- resources/views/admin/components/livewire/info-card-siswa.blade.php
+++ resources/views/admin/components/livewire/info-card-siswa.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="col-12 col-lg-4 col-md-6">
+    <div class="col-6 col-lg-4 col-md-6">
         <div class="card">
             <div class="card-body px-4 py-4-5">
                 <div class="row">
@@ -34,7 +34,7 @@
                         </div>
                     </div>
                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7 text-center md-text-start">
-                        <h6 class="text-muted font-semibold">Active</h6>
+                        <h6 class="text-muted font-semibold">Aktif</h6>
                         <h6 class="font-extrabold mb-0">{{$siswaActive}}</h6>
                     </div>
                 </div>
@@ -55,8 +55,50 @@
                         </div>
                     </div>
                     <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7 text-center md-text-start">
-                        <h6 class="text-muted font-semibold">Inactive</h6>
-                        <h6 class="font-extrabold mb-0">{{$siswaInactive}}</h6>
+                        <h6 class="text-muted font-semibold">Lulus</h6>
+                        <h6 class="font-extrabold mb-0">{{$siswaLulus}}</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-4 col-md-6">
+        <div class="card">
+            <div class="card-body px-4 py-4-5">
+                <div class="row">
+                    <div
+                        class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-center md-justify-content-start"
+                    >
+                        <div class="stats-icon green mb-2">
+                            <div data-bs-toggle="tooltip" data-bs-placement="top" title="Siswa Inaktif">
+                                <i class="bi bi-people-fill"></i>
+                            </div>   
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7 text-center md-text-start">
+                        <h6 class="text-muted font-semibold">Pindah</h6>
+                        <h6 class="font-extrabold mb-0">{{$siswaPindah}}</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-6 col-lg-4 col-md-6">
+        <div class="card">
+            <div class="card-body px-4 py-4-5">
+                <div class="row">
+                    <div
+                        class="col-md-4 col-lg-12 col-xl-12 col-xxl-5 d-flex justify-content-center md-justify-content-start"
+                    >
+                        <div class="stats-icon blue mb-2">
+                            <div data-bs-toggle="tooltip" data-bs-placement="top" title="Siswa Inaktif">
+                                <i class="bi bi-people-fill"></i>
+                            </div>   
+                        </div>
+                    </div>
+                    <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7 text-center md-text-start">
+                        <h6 class="text-muted font-semibold">Drop Out</h6>
+                        <h6 class="font-extrabold mb-0">{{$siswaDO}}</h6>
                     </div>
                 </div>
             </div>
