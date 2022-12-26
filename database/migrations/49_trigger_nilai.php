c5ff189c3f4427ee44a47526528ed1d022d5cf93
HelloitsMutiaa <rahmahm868@gmail.com>
2022-12-26 12:44:21 +0700

    fix trigger nilai part2

--- database/migrations/49_trigger_nilai.php
+++ database/migrations/49_trigger_nilai.php
@@ -69,7 +69,7 @@ return new class extends Migration
         BEGIN
             IF(NEW.nilai_id <> OLD.nilai_id OR NEW.sesi <> OLD.sesi OR NEW.mapel <> OLD.mapel OR NEW.guru <> OLD.guru OR NEW.kontrak_siswa <> OLD.kontrak_siswa OR NEW.jenis <> OLD.jenis OR NEW.kkm_aktif <> OLD.kkm_aktif)THEN
                 SIGNAL SQLSTATE "45000"
-                SET MESSAGE_TEXT = "Tidak dapat mengubah role";
+                SET MESSAGE_TEXT = "Tidak dapat mengubah nilai";
             END IF;
         END
         ');
@@ -98,5 +98,6 @@ return new class extends Migration
         DB::unprepared('DROP TRIGGER log_delete_nilai');
         DB::unprepared('DROP TRIGGER validasi_input_nilai');
        /*  DB::unprepared('DROP TRIGGER validasi_nilai'); */
+       DB::unprepared('DROP TRIGGER disable_update_nilai');
     }
 };
