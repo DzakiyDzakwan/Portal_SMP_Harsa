afee0cc991ded9c1fdeb7ace1ad363cf4872b28b
talitha <talithasyafiyah1112@gmail.com>
2022-12-28 01:19:24 +0700

    admin CRD - U nya belom

---
+++ app/Http/Livewire/CreateModalAdmin.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+
+class CreateModalAdmin extends Component
+{
+    public $username, $password;
+
+    protected $rules = [
+        'username' => 'required|min:5',
+        'password' => 'required'  
+    ];
+
+    public function updated($fields) {
+        $this->validateOnly($fields);
+    }
+
+    public function store() {
+
+        $this->validate([
+            'username' => 'required|min:5',
+            'password' => 'required'  
+        ]);
+
+        $this->password = Hash::make($this->password);
+
+        DB::select('CALL add_admin(?, ?, ?)', [$this->username, $this->password, auth()->user()->uuid]);
+        $this->reset();
+        $this->emit('adminStore');
+        $this->dispatchBrowserEvent('close-create-modal');
+        $this->dispatchBrowserEvent('insert-alert');
+    }
+
+    public function render()
+    {
+        return view('livewire.create-modal-admin');
+    }
+}
