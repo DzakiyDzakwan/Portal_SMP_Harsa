3fefd4b41464b8cf3e323011a2f42e6d31363acf
putrastg12 <putrastg57@gmail.com>
2023-01-01 21:30:10 +0700

    deni

---
+++ app/Http/Controllers/ManajemenKelasController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManajemenKelasController extends Controller
+{
+    public function mapel() {
+        $pages = 'manajemenKelas';
+        return  view('admin.mapel', [
+            'pages' => $pages,
+        ]);
+    }
+
+    public function kelas()
+    {
+        $pages = 'manajemenKelas';
+        return  view('admin.kelas', [
+            'pages' => $pages,
+        ]);
+    }
+    public function ekskul()
+    {
+        $pages = 'manajemenEkskul';
+        return  view('admin.ekskul', [
+            'pages' => $pages,
+        ]);
+    }
+}
--- app/Http/Controllers/siswa/ProfilController.php
+++ app/Http/Controllers/siswa/ProfilController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\siswa;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Siswa;
 
 class ProfilController extends Controller
 {
@@ -21,5 +22,27 @@ class ProfilController extends Controller
         return view('siswa.editProfil', [
             'pages' => $pages
         ]);
+    }
+
+    public function updateProfilSiswa(Request $request)
+    {
+        // dd($request);
+        $validated = $request->validate([
+            
+            'kelas_awal' => '',
+            'anak_ke' => '',
+            'nama_ayah' => '',
+            'pekerjaan_ayah' => '',
+            'nama_ibu' => '',
+            'pekerjaan_ibu' => '',
+            'alamat_orangtua' => '',
+            'telepon_orangtua' => '',
+            'nama_wali' => '',
+            'pekerjaan_wali' => '',
+            'telepon_wali' => '',
+        ]);
+        // dd($validated);
+        Siswa::where('user', auth()->user()->uuid)->update($validated);
+        return "oke";
     }
 }
